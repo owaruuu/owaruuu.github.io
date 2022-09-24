4444d122f3a1e1f922ebde3be396552322bd0038
owaruuu <owaruuu@gmail.com>
2022-09-24 01:56:30 -0300

    add kana struct
    add build kana card from structs
    add answer check on enter input

--- script.js
+++ script.js
@@ -1,22 +1,84 @@
+let kanas = [
+    {
+        kana : 'か',
+        answer : 'ka',
+    },
+    {
+        kana : 'き',
+        answer : 'ki',
+    },
+    {
+        kana : 'く',
+        answer : 'ku',
+    },
+    {
+        kana : 'け',
+        answer : 'ke',
+    },
+    {
+        kana : 'こ',
+        answer : 'ko',
+    },
+]
+
+const container = document.querySelector('.container');
+// const input = document.querySelector('.input');
+
+kanas.forEach(kana => {
+    BuildCard(kana)
+});
+
+function BuildCard(kana){
+    let cardDiv = document.createElement('div');
+    container.appendChild(cardDiv);
+    cardDiv.classList.add('card');
+    cardDiv.setAttribute('data-answer', kana.answer);
+    let question = document.createElement('div');
+    cardDiv.appendChild(question);
+    question.classList.add('question');
+    question.textContent = kana.kana;
+    let form = document.createElement('form');
+    cardDiv.appendChild(form);
+    form.classList.add('form');
+    let input = document.createElement('input'); 
+    form.appendChild(input);
+    input.type = 'text';
+    input.autocomplete = 'off';
+    input.size = 4;
+    input.maxLength = 5;
+}
+
 const forms = document.querySelectorAll('.form');
-const input = document.querySelector('.input');
 
 forms.forEach(form => {
     form.addEventListener('submit', Submit);
 });
 
-
 function Submit(event){
-    // event.form.log
-    console.log(event.target[0].value);
-    console.log(event.target.parentElement);
-    console.log(event.target[0]);
-    console.log(event);
+    let cardDiv = event.target.parentElement;
+    let form = event.target;
+    let input = event.target[0];
+    let inputValue = event.target[0].value;
+    let answer = cardDiv.dataset.answer;
     
-    // event.target[0].readOnly = true;
-    event.target[0].disabled = true;
-    event.target.parentElement.classList.add('correct');
-    event.target.parentElement.setAttribute('data-some', 20);
+    if(inputValue == answer )
+    {
+        event.target[0].disabled = true;
+        form.classList.remove('incorrect');
+        form.classList.add('correct');
+        //pass focus
+    }else{
+        form.classList.add('incorrect');
+        input.value = '';
+    }
+
+    //console.log(event.target[0].value);
+    //console.log(event.target.parentElement);
+    //console.log(event.target[0]);
+    console.log(event);
+    //console.log(cardDiv.dataset.answer);
+    
+    //cardDiv.setAttribute('data-some', 20);
     
     event.preventDefault();
 }

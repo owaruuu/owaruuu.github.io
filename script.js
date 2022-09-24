65c7ead0238057331f3ac8c8f92374a30af1f31e
owaruuu <owaruuu@gmail.com>
2022-09-23 21:22:30 -0300

    modify card on submit

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const form = document.querySelector('.form');
+const forms = document.querySelectorAll('.form');
 const input = document.querySelector('.input');
 
-form.addEventListener('submit', Submit);
+forms.forEach(form => {
+    form.addEventListener('submit', Submit);
+});
+
 
 function Submit(event){
     // event.form.log
-    console.log(input.value);
+    console.log(event.target[0].value);
+    console.log(event.target.parentElement);
+    console.log(event.target[0]);
+    console.log(event);
+    
+    // event.target[0].readOnly = true;
+    event.target[0].disabled = true;
+    event.target.parentElement.classList.add('correct');
+    event.target.parentElement.setAttribute('data-some', 20);
+    
     event.preventDefault();
 }
